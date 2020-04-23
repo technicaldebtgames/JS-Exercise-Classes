ed1e07169c3f690b960a9bf5cf858a823ad7e2bd
Dan Brioli <technicaldebtgames@gmail.com>
2020-04-23 18:31:11 -0400

    uhh... everything but stretch

--- index.js
+++ index.js
@@ -42,6 +42,37 @@ class Airplane {
 
 class Person {
 
+  constructor(name, age){
+
+    this.name = name;
+    this.age = age;
+
+    this.stomach = [];
+
+  }
+
+  eat(edible) {
+
+    if (this.stomach.length < 10) {
+
+      this.stomach.push(edible);
+
+    }
+
+  }
+
+  poop() {
+
+    this.stomach = [];
+
+  }
+
+  toString() {
+
+    return this.name + ", " + this.age;
+
+  }
+
 }
 
 /*
@@ -60,6 +91,43 @@ class Person {
 
 class Car {
 
+  constructor(model, milesPerGallon) {
+
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+
+    this.tank = 0;
+    this.odometer = 0;
+
+  }
+
+  fill(gallons) {
+
+    this.tank += gallons;
+
+  }
+
+  drive(distance) {
+
+    let dActual = this.tank * this.milesPerGallon;
+
+    if (distance < dActual) {
+
+      this.odometer += distance;
+      this.tank -= (distance / this.milesPerGallon);
+
+    }
+    else {
+
+      this.odometer += dActual;
+      this.tank = 0;
+
+      return "I ran out of fuel at " + this.odometer + " miles!";
+
+    }
+
+  }
+
 }
 
 /*
@@ -75,6 +143,20 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+
+  constructor(args) {
+
+    this.name = args.name;
+    this.age = args.age;
+    this.location = args.location;
+
+  }
+
+  speak() {
+
+    return "Hello my name is " + this.name + ", I am from " + this.location;
+
+  }
 
 }
 
@@ -92,7 +174,29 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+
+  constructor(args) {
+    
+    super(args);
+    
+    this.specialty = args.specialty;
+    this.favLanguage = args.favLanguage;
+    this.catchPhrase = args.catchPhrase;
+
+  }
+
+  demo(subject) {
+
+    return "Today we are learning about " + subject;
+
+  }
+
+  grade(student, subject) {
+
+    return student.name + " receives a perfect score on " + subject;
+
+  }
 
 }
 
@@ -111,7 +215,45 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+
+  constructor(args) {
+
+    super(args);
+
+    this.previousBackground = args.previousBackground;
+    this.className = args.className;
+    this.favSubjects = args.favSubjects;
+
+  }
+
+  listSubjects() {
+
+    let subjectString = "Loving ";
+
+    for (let i = 0; i < this.favSubjects.length - 1; i++){
+
+      subjectString += this.favSubjects[i] + ", ";
+
+    }
+
+    subjectString += this.favSubjects[this.favSubjects.length - 1] + "!";
+
+    return subjectString;
+
+  }
+
+  PRAssignment(subject) {
+
+    return this.name + " has submitted a PR for " + subject;
+
+  }
+
+  sprintChallenge(subject) {
+
+    return this.name + " has begun sprint challenge on " + subject;
+
+  }
 
 }
 
@@ -128,7 +270,28 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+
+  constructor(args) {
+
+    super(args);
+
+    this.gradClassName = args.gradClassName;
+    this.favInstructor = args.favInstructor;
+
+  }
+
+  standUp(channel) {
+
+    return this.name + " announces to " + channel + ", @channel standy times!";
+
+  }
+
+  debugsCode(student, subject) {
+
+    return this.name + " debugs " + student.name + "'s code on " + subject;
+
+  }
 
 }
 
